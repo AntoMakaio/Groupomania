759e30fba0ea3e1c6581f9b6dc3d60620578b0d4
AntoMakaio <antonin.begue@live.fr>
2022-06-15 17:55:19 +0200

    router get ID userInfo + user_controller

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth_controller");
+const userController = require("../controllers/user_controller");
 
+// Autentification
 router.post("/register", authController.signUp);
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
 
 module.exports = router;
