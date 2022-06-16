c87a71d6a0621435069c551a4fcc02fb204419fc
AntoMakaio <antonin.begue@live.fr>
2022-06-16 17:56:09 +0200

    check user * get

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user_routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/database");
+const { checkUser } = require("./middleware/auth_middleware");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//middleware
+//secure all - jsonwebtoken
+app.get("*", checkUser);
 
 //routes
 app.use("/api/user", userRoutes);
