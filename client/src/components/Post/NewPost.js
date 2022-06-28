d693af158b9673b65da33504c29f326332007994
AntoMakaio <antonin.begue@live.fr>
2022-06-28 23:17:53 +0200

    post form

--- client/src/components/Post/NewPost.js
+++ client/src/components/Post/NewPost.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { isEmpty } from "../Utils";
+import { isEmpty, timestampParser } from "../Utils";
 import { NavLink } from "react-router-dom";
 
 const NewPost = () => {
@@ -11,9 +11,35 @@ const NewPost = () => {
   const [file, setFile] = useState();
   const userData = useSelector((state) => state.userReducer);
 
+  const handlePicture = () => {};
+  const handlePost = () => {};
+  const cancelPost = () => {
+    setMessage("");
+    setPostPicture("");
+    setVideo("");
+    setFile("");
+  };
+
+  const handleVideo = () => {
+    let findLink = message.split(" ");
+    for (let i = 0; i < findLink.length; i++) {
+      if (
+        findLink[i].includes("https://www.yout") ||
+        findLink[i].includes("https://yout")
+      ) {
+        let embed = findLink[i].replace("watch?v=", "embed/");
+        setVideo(embed.split("&")[0]);
+        findLink.splice(i, 1);
+        setMessage(findLink.join(" "));
+        setPostPicture("");
+      }
+    }
+  };
+
   useEffect(() => {
     if (!isEmpty(userData)) setIsLoading(false);
-  }, [userData]);
+    handleVideo();
+  }, [userData, message, video]);
 
   return (
     <div className="post-container">
@@ -34,6 +60,63 @@ const NewPost = () => {
               onChange={(e) => setMessage(e.target.value)}
               value={message}
             />
+            {message || postPicture || video.length > 20 ? (
+              <li className="card-container">
+                <div className="card-left">
+                  <img src={userData.picture} alt="user picture" />
+                </div>
+                <div className="card-right">
+                  <div className="card-header">
+                    <div className="pseudo">
+                      <h3>{userData.pseudo}</h3>
+                    </div>
+                    <span>{timestampParser(Date.now())}</span>
+                  </div>
+                  <div className="content">
+                    <p>{message}</p>
+                    <img src={postPicture} alt="" />
+                    {video && (
+                      <iframe
+                        src={video}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title={video}
+                      ></iframe>
+                    )}
+                  </div>
+                </div>
+              </li>
+            ) : null}
+            <div className="footer-form">
+              <div className="icon">
+                {isEmpty(video) && (
+                  <>
+                    <img src="./img/icons/picture.svg" alt="picture" />
+                    <input
+                      type="file"
+                      id="file-upload"
+                      name="file"
+                      accept=".jpeg, .jpg, .png"
+                      onChange={(e) => handlePicture(e)}
+                    />
+                  </>
+                )}
+                {video && (
+                  <button onClick={() => setVideo("")}>Supprimer vidéo</button>
+                )}
+              </div>
+              <div className="btn-send">
+                {message || postPicture || video.length > 20 ? (
+                  <button className="cancel" onClick={cancelPost}>
+                    Annuler le post
+                  </button>
+                ) : null}
+                <button className="send" onClick={handlePost}>
+                  Envoyer
+                </button>
+              </div>
+            </div>
           </div>
         </>
       )}
